92a9d1228411663910e1619401c43b60eb184aa2
mariano894 <mariano894@gmail.com>
2021-10-24 08:53:50 -0600

    uncomment login blocks

--- app/src/main/java/com/umg/primarydetail01/ui/login/LoginActivity.java
+++ app/src/main/java/com/umg/primarydetail01/ui/login/LoginActivity.java
@@ -27,7 +27,8 @@ import com.umg.primarydetail01.ItemDetailHostActivity;
 import com.umg.primarydetail01.R;
 import com.umg.primarydetail01.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity 
+{
 
     public static final String EXTRA_MESSAGE = "USER_EMAIL_ON_MESSAGE";
     public static String user_email = "";
@@ -36,7 +37,8 @@ public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+	{
         super.onCreate(savedInstanceState);
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
@@ -50,10 +52,13 @@ public class LoginActivity extends AppCompatActivity {
         final Button loginButton = binding.login;
         final ProgressBar loadingProgressBar = binding.loading;
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() 
+		{
             @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
+            public void onChanged(@Nullable LoginFormState loginFormState) 
+			{
+                if (loginFormState == null) 
+				{
                     return;
                 }
                 loginButton.setEnabled(loginFormState.isDataValid());
@@ -66,9 +71,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() 
+		{
             @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
+            public void onChanged(@Nullable LoginResult loginResult) 
+			{
                 if (loginResult == null) {
                     return;
                 }
@@ -86,29 +93,35 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        TextWatcher afterTextChangedListener = new TextWatcher() {
+        TextWatcher afterTextChangedListener = new TextWatcher() 
+		{
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) 
+			{
                 // ignore
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public void onTextChanged(CharSequence s, int start, int before, int count) 
+			{
                 // ignore
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
+            public void afterTextChanged(Editable s) 
+			{
                 loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
             }
         };
         usernameEditText.addTextChangedListener(afterTextChangedListener);
         passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() 
+		{
 
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) 
+			{
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
                     loginViewModel.login(usernameEditText.getText().toString(),
                             passwordEditText.getText().toString());
@@ -117,7 +130,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() 
+		{
             @Override
             public void onClick(View v) {
                 loadingProgressBar.setVisibility(View.VISIBLE);
@@ -133,69 +147,21 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
+    private void updateUiWithUser(LoggedInUserView model) 
+	{
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
-    private void showLoginFailed(@StringRes Integer errorString) {
+    private void showLoginFailed(@StringRes Integer errorString) 
+	{
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
-    private void justAfterLogin() {
-        /*Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://pokeapi.co/api/v2/")
-                .addConverterFactory(GsonConverterFactory.create(
-                        new GsonBuilder().serializeNulls().create()
-                ))
-                .build();
-        PokemonAPIService pokemonApiService = retrofit.create(PokemonAPIService.class);
-        Call<PokemonFetchResults> call = pokemonApiService.getPokemons();
-
-        call.enqueue(new Callback<PokemonFetchResults>() {
-            //@RequiresApi(api = Build.VERSION_CODES.N)
-            @Override
-            public void onResponse(Call<PokemonFetchResults> call,
-                                   Response<PokemonFetchResults> response) {
-                if (response.isSuccessful()) {
-                    ArrayList pokemonList = response.body().getResults();
-
-                    for(int i = 1; i <= 10; i++)
-                    {
-                        //Pokemon pokemon = (Pokemon)pokemonList.get(i);
-
-                        Object getrow = pokemonList.get(i);
-                        LinkedTreeMap<Object,Object> t = (LinkedTreeMap) getrow;
-                        String name = t.get("name").toString();
-
-                        System.out.println(name);
-
-                        //trying to create on item
-                        PlaceholderContent.startOneItem(i, name, "It's a " + name + "!");
-
-                    }
-
-                   // View recyclerView = findViewById(R.id.item_list);
-                    //assert recyclerView != null;
-                    //setupRecyclerView((RecyclerView) recyclerView, pokemonList);
-                } else {
-                    Log.d("Error", "Something happened");
-                    return;
-                }
-            }
-
-            @Override
-            public void onFailure(Call call, Throwable t) {
-                Log.d("Error", t.toString());
-            }
-        });*/
-
-
-
-        Intent intent = new Intent(this, ItemDetailHostActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        //String message = editText.getText().toString();
+    private void justAfterLogin() 
+	{       
+        Intent intent = new Intent(this, ItemDetailHostActivity.class);        
         String message = "Start the Item Detail Host Activity";
         intent.putExtra(EXTRA_MESSAGE, user_email);
         startActivity(intent);
